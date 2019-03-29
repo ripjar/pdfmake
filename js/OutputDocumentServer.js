1bc4779456ca8a20f7aa3d5f2a207e1dc1d36ecd
jonathaneverard <jonathan.everard@ripjar.com>
2019-03-29 09:49:17 +0000

    Build of fonts. includes build files too...

---
+++ js/OutputDocumentServer.js
@@ -0,0 +1,21 @@
+"use strict";
+
+exports.__esModule = true;
+exports.default = void 0;
+
+var _OutputDocument = _interopRequireDefault(require("./OutputDocument"));
+
+var _fs = _interopRequireDefault(require("fs"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+class OutputDocumentServer extends _OutputDocument.default {
+  write(filename) {
+    this.getStream().pipe(_fs.default.createWriteStream(filename));
+    this.getStream().end();
+  }
+
+}
+
+var _default = OutputDocumentServer;
+exports.default = _default;
