7673a723ee7da4c8bd949879973671538706470c
alun-turner <alun.turner@ripjar.com>
2021-07-02 17:07:30 +0100

    get basics pdf running for testing

--- examples/basics.js
+++ examples/basics.js
@@ -1,23 +1,46 @@
 var fonts = {
+	NotoSansArabic: {
+		normal: "examples/fonts/NotoSansEnglishArabic-reg.ttf",
+		bold: "examples/fonts/NotoSansEnglishArabic-Bold.ttf",
+		italics: "examples/fonts/NotoSansEnglishArabic-reg.ttf",
+		bolditalics: "examples/fonts/NotoSansEnglishArabic-Bold.ttf",
+	},
 	Roboto: {
-		normal: 'fonts/Roboto-Regular.ttf',
-		bold: 'fonts/Roboto-Medium.ttf',
-		italics: 'fonts/Roboto-Italic.ttf',
-		bolditalics: 'fonts/Roboto-MediumItalic.ttf'
-	}
+		normal: "examples/fonts/Roboto-Regular.ttf",
+		bold: "examples/fonts/Roboto-Medium.ttf",
+		italics: "examples/fonts/Roboto-Italic.ttf",
+		bolditalics: "examples/fonts/Roboto-MediumItalic.ttf",
+	},
 };
 
-var PdfPrinter = require('../src/printer');
+var PdfPrinter = require("../src/printer");
 var printer = new PdfPrinter(fonts);
-var fs = require('fs');
+var fs = require("fs");
+
+var RLE = String.fromCodePoint();
 
 var docDefinition = {
 	content: [
-		'First paragraph',
-		'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
-	]
+		{
+			text: [
+				{ text: "First paragraph " },
+				{ text: "some arabic next " },
+				{ text: "ثلاث كلمات في ", font: "NotoSansArabic", rtl: true },
+				" Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
+				{
+					stack: [{ text: "ثلاث كلمات في ", font: "NotoSansArabic" }],
+					rtl: true,
+				},
+			],
+		},
+		{
+			text: "ثلاث كلمات في ",
+			font: "NotoSansArabic",
+			rtl: true,
+		},
+	],
 };
 
 var pdfDoc = printer.createPdfKitDocument(docDefinition);
-pdfDoc.pipe(fs.createWriteStream('pdfs/basics.pdf'));
+pdfDoc.pipe(fs.createWriteStream("examples/pdfs/basics.pdf"));
 pdfDoc.end();
